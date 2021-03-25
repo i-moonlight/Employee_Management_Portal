6acf16997e8bf7b88d971f749e6988eb1143c52a
i-moonlight <infinite.moonlight0506@gmail.com>
2021-03-25 18:48:34 +0200

    feat(client): app routing module

---
+++ WebClient/src/app/modules/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from '../components/employee/employee.component';
+import { DepartmentComponent } from '../components/department/department.component';
+
+const routes: Routes = [
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'department', component: DepartmentComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
