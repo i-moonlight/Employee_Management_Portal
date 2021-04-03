2ce98ac6094fa5ade0566bfdfb0be1f0bba755f5
i-moonlight <infinite.moonlight0506@gmail.com>
2021-04-03 18:04:39 +0200

    test: test method GET in department controller

---
+++ WebAPI/WebAPI.Tests/DepartmentControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using WebAPI.Controllers;
+using WebAPI.Models;
+using WebAPI.Repositories.Interfaces;
+
+namespace WebAPI.Tests
+{
+    [TestFixture]
+    public class DepartmentControllerTest
+    {
+        private Mock<ICrudRepository<Department>> _mockDepRepo;
+        private DepartmentController _controller;
+        private IEnumerable<Department> _fakeCategories;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockDepRepo = new Mock<ICrudRepository<Department>>();
+            _controller = new DepartmentController(_mockDepRepo.Object);
+            _fakeCategories = GetCategories();
+        }
+
+        [Test]
+        public void Get_ShouldReturnAllDepartments()
+        {
+            // Arrange.   
+            _mockDepRepo.Setup(x => x.Read()).Returns(_fakeCategories);
+
+            // Act.
+            JsonResult result = _controller.Get();
+
+            // Assert.
+            Assert.NotNull(result, "Result is null");
+            Assert.AreEqual(new JsonResult(new Department()).GetType(), result.GetType(), "Return type mismatch");
+            Assert.AreEqual(typeof(List<Department>), result.Value.GetType(), "Return value type mismatch");
+        }
+
+        private static IEnumerable<Department> GetCategories()
+        {
+            var fakeCategories = new List<Department>
+            {
+                new Department {DepartmentId = 1, DepartmentName = "Test1"}
+            }.AsEnumerable();
+            return fakeCategories;
+        }
+    }
+}
