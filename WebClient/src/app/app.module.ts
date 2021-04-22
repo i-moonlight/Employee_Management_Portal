4d740100d62d5a86024a0a127d435b68491ff5a1
i-moonlight <infinite.moonlight0506@gmail.com>
2021-04-22 19:02:26 +0200

    feat: employee modal component

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeComponent } from './components/employee/emp.comp';
 import { EmployeeListComponent } from './components/employee/emp-list/emp-list.comp';
+import { EmployeeModalComponent } from './components/employee/emp-modal/emp-modal.comp';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     EmployeeListComponent,
+    EmployeeModalComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WebClient/src/app/components/employee/emp-modal/emp-modal.comp.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-modal',
+  templateUrl: './emp-modal.comp.html',
+  styleUrls: ['./emp-modal.comp.css']
+})
+export class EmployeeModalComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
