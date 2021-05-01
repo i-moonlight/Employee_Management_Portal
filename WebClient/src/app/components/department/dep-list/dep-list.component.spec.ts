4e5cee7b1ba0b451f6d4920caebc27d8d3091cf4
i-moonlight <infinite.moonlight0506@gmail.com>
2021-05-01 20:39:31 +0200

    test: refactor tests in department list component

--- WebClient/src/app/components/department/dep-list/dep-list.component.spec.ts
+++ WebClient/src/app/components/department/dep-list/dep-list.component.spec.ts
@@ -2,15 +2,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { DepartmentListComponent } from './dep-list.component';
 import { SharedService } from '../../../services/shared/shared.service';
-import { of } from 'rxjs';
+import { Department } from '../department.component';
 
 describe('DepartmentListComponent', () => {
   let component: DepartmentListComponent;
   let fixture: ComponentFixture<DepartmentListComponent>;
   let service: SharedService;
-  let mockList: any[];
+  let mockList: Department[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,7 +20,7 @@ describe('DepartmentListComponent', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
-  });
+  })
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DepartmentListComponent);
@@ -27,21 +28,39 @@ describe('DepartmentListComponent', () => {
     service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
     mockList = [];
     fixture.detectChanges();
-  });
+  })
 
   it('should create department list component', () => {
     expect(component).toBeTruthy();
-  });
+  })
+
+  it('should deactivate department modal component when initialize department list component', () => {
+    expect(component.activateDepModalComp).toBeFalse();
+  })
+
+  it('should set department filter id value when initialize department list component', () => {
+    expect(component.departmentIdFilter).toEqual('');
+  })
+
+  it('should set department filter name value when initialize department list component', () => {
+    expect(component.departmentNameFilter).toEqual('');
+  })
+
+  it('should set call update department list when initialize department list component', () => {
+    const spy = spyOn(component, 'updateDepartmentList');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  })
 
   it('should call shared service when update department list', () => {
-    const spy = spyOn(service, 'getDepartmentList').and.returnValue(of(mockList));
+    const spy = spyOn(service, 'getDepartmentListFromDB').and.returnValue(of(mockList));
     component.updateDepartmentList();
     expect(spy.calls.any()).toBeTruthy();
-  });
+  })
 
   it('should set department list value when update department list', () => {
-    spyOn(service, 'getDepartmentList').and.returnValue(of(mockList));
+    spyOn(service, 'getDepartmentListFromDB').and.returnValue(of(mockList));
     component.updateDepartmentList();
     expect(component.departmentList).toEqual(mockList);
-  });
-});
+  })
+})
