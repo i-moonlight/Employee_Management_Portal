3aba94a97313bcbac6ff99067a102f02b824d77b
i-moonlight <infinite.moonlight0506@gmail.com>
2021-05-04 19:12:46 +0200

    feat: employee modal in employee list component

--- WebClient/src/app/components/employee/emp-modal/emp-modal.comp.ts
+++ WebClient/src/app/components/employee/emp-modal/emp-modal.comp.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-emp-modal',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp-modal.comp.css']
 })
 export class EmployeeModalComponent implements OnInit {
+  @Input() emp: any;
 
   constructor() {}
 
