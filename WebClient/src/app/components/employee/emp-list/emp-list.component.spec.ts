45b3bff15e543b611199c9feced3f7cb56c1791c
i-moonlight <infinite.moonlight0506@gmail.com>
2021-05-17 19:22:08 +0200

    test: employee list component tests

--- WebClient/src/app/components/employee/emp-list/emp-list.component.spec.ts
+++ WebClient/src/app/components/employee/emp-list/emp-list.component.spec.ts
@@ -2,10 +2,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
+import { Employee } from '../employee.component';
 import { EmployeeListComponent } from './emp-list.component';
 import { SharedService } from '../../../services/shared/shared.service';
-import { Employee } from '../employee.component';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
@@ -19,8 +20,7 @@ describe('EmployeeListComponent', () => {
       declarations: [EmployeeListComponent],
       imports: [HttpClientModule, FormsModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -34,23 +34,46 @@ describe('EmployeeListComponent', () => {
 
   it('should create employee list component', () => {
     expect(component).toBeTruthy();
-  });
+  })
 
   it('should call shared service when update employee list', () => {
     const spy = spyOn(service, 'getEmployeeListFromDB').and.returnValue(of(mockList));
     component.updateEmployeeList();
     expect(spy.calls.any()).toBeTruthy();
-  });
+  })
 
   it('should set employee list value when update employee list', () => {
     spyOn(service, 'getEmployeeListFromDB').and.returnValue(of(mockList));
     component.updateEmployeeList();
     expect(component.employeeList).toEqual(mockList);
-  });
+  })
 
+  it('should call show employee photo method with param', () => {
+    const spy = spyOn(component, 'showEmployeePhoto').and.returnValue('path');
+    component.showEmployeePhoto(mock);
+    expect(spy).toHaveBeenCalledWith(mock);
+  })
+
+  it('should set employee value when show employee photo', () => {
+    component.showEmployeePhoto(mock);
+    expect(component.employee).toEqual(mock);
+  })
+
+  it('should call close employee modal method when click on button', () => {
+    const spy = spyOn(component, 'closeEmployeeModal');
+    const btn = fixture.debugElement.query(By.css('.btn-close'));
+    btn.triggerEventHandler('click', null);
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should set component activate value when close employee modal', () => {
+    component.closeEmployeeModal();
+    expect(component.activateAddEditEmpComp).toBeFalse();
+  });
+  
   it('should call confirm window when show confirm', () => {
     const spy = spyOn(window, 'confirm');
     component.showConfirmDeleteEmployee(mock);
     expect(spy).toHaveBeenCalled();
-  });
-});
+  })
+})
