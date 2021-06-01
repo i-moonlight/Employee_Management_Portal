0a891dfb4af09ad727cba3d68cdbeda2820f6c5c
i-moonlight <infinite.moonlight0506@gmail.com>
2021-06-01 19:05:17 +0200

    test: test call update employee list service in employee list component

--- WebClient/src/app/components/employee/emp-list/emp-list.comp.spec.ts
+++ WebClient/src/app/components/employee/emp-list/emp-list.comp.spec.ts
@@ -1,13 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EmployeeListComponent } from './emp-list.comp';
+import { SharedService } from '../../../services/shared/shared.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
+  let service: SharedService;
+  let mockList: string[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EmployeeListComponent]
+      declarations: [EmployeeListComponent],
+      imports: [HttpClientModule, FormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   });
@@ -15,10 +24,18 @@ describe('EmployeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
+    service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
+    mockList = [];
     fixture.detectChanges();
   });
 
   it('should create employee list component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call shared service when update employee list', () => {
+    const spy = spyOn(service, 'getEmployeeListFromDB').and.returnValue(of(mockList));
+    component.updateEmployeeList();
+    expect(spy.calls.any()).toBeTruthy();
   });
 });
