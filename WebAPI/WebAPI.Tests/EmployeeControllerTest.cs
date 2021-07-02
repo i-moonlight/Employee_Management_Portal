33dcdd04e4416cc16a1f7de384fe4b058a967879
i-moonlight <infinite.moonlight0506@gmail.com>
2021-07-02 19:34:47 +0200

    build(server): api tests project

---
+++ WebAPI/WebAPI.Tests/EmployeeControllerTest.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using WebAPI.Controllers;
+using WebAPI.Models;
+using WebAPI.Repositories.Interfaces;
+
+namespace WebAPI.Tests
+{
+    [TestFixture]
+    public class EmployeeControllerTest
+    {
+        private Mock<ICrudRepository<Employee>> _mockEmpRepo;
+        private EmployeeController _controller;
+        private IEnumerable<Employee> _fakeCategories;
+        private Employee _model;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockEmpRepo = new Mock<ICrudRepository<Employee>>();
+            _controller = new EmployeeController(_mockEmpRepo.Object);
+            _fakeCategories = GetCategories();
+            _model = new Employee();
+        }
+
+        [Test]
+        public void Get_ShouldReturnAllEmployees()
+        {
+            // Arrange   
+            _mockEmpRepo.Setup(x => x.Read()).Returns(_fakeCategories);
+
+            // Act
+            JsonResult result = _controller.Get();
+
+            // Assert
+            Assert.NotNull(result, "Result is null");
+            Assert.AreEqual(new JsonResult(new Employee()).GetType(), result.GetType(), "Return type mismatch");
+            Assert.AreEqual(typeof(List<Employee>), result.Value.GetType(), "Return value type mismatch");
+        }
+        
+        [Test]
+        public void Post_ShouldCreateEmployee()
+        {
+            // Arrange.   
+            _mockEmpRepo.Setup(x => x.Create(_model)).Returns(_fakeCategories.GetEnumerator().Current);
+
+            // Act.
+            JsonResult result = _controller.Post(_model);
+
+            // Assert.
+            Assert.NotNull(result, "Result is null");
+            Assert.AreEqual(new JsonResult(_model).GetType(), result.GetType(), "Return type mismatch");
+            Assert.AreEqual(typeof(string), result.Value.GetType(), "Return value type mismatch");
+        }
+
+        private static IEnumerable<Employee> GetCategories()
+        {
+            var fakeCategories = new List<Employee>
+            {
+                new Employee {EmployeeId = 1, EmployeeName = "Test1"}
+            }.AsEnumerable();
+            return fakeCategories;
+        }
+    }
+}
