7e406f7a3eef55ccfb65814066a813eb8f67d497
i-moonlight <infinite.moonlight0506@gmail.com>
2021-07-23 19:46:38 +0200

    feat: user registration
    - Add user registration method in authentication controller
    
    - Create response model
    
    - Create register user view model

---
+++ WebAPI/auth/WebAPI.Authentication/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Authentication.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WebAPI/auth/WebAPI.Authentication/ViewModels/ResponseModel.cs
@@ -0,0 +1,27 @@
+﻿namespace WebAPI.Authentication.ViewModels
+{
+    public class ResponseModel
+    {
+        public ResponseModel
+        (
+            ResponseCode responseCode,
+            string responseMessage,
+            object dataSet
+        )
+        {
+            ResponseCode = responseCode;
+            ResponseMessage = responseMessage;
+            DateSet = dataSet;
+        }
+
+        public ResponseCode ResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+        public object DateSet { get; set; }
+    }
+
+    public enum ResponseCode
+    {
+        Ok = 1,
+        Error = 2
+    }
+}
