706957c3aeb369c60004f9ac84b2541b201bb685
i-moonlight <infinite.moonlight0506@gmail.com>
2021-07-23 19:08:26 +0200

    feat: generate token user authentication
    - Add generate token method in authentication controller
    
    - Add identity configuration in startup

--- WebAPI/auth/WebAPI.Authentication/Startup.cs
+++ WebAPI/auth/WebAPI.Authentication/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,8 +8,53 @@ namespace WebAPI.Authentication
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) {}
-        
+        /// <summary>
+        /// This method gets called by the runtime.
+        /// Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            #region Role Identity
+
+            services.AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequireDigit = true;
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireUppercase = true;
+                    options.Lockout.MaxFailedAccessAttempts = 6;
+                    options.User.RequireUniqueEmail = true;
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.SignIn.RequireConfirmedEmail = false;
+                })
+                .AddRoles<IdentityRole>()
+                .AddRoleManager<RoleManager<IdentityRole>>()
+                .AddSignInManager<SignInManager<User>>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            #endregion
+
+            #region CORS
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            #endregion
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime.
+        /// Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -19,11 +64,13 @@ namespace WebAPI.Authentication
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                    await context.Response.WriteAsync("Hello World!"));
-            });
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
