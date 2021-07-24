b6d27ec3cac08d71482abc47120f77dbce77f72d
i-moonlight <infinite.moonlight0506@gmail.com>
2021-07-24 17:52:12 +0200

    refactor: employee modal component

--- WebClient/src/app/components/employee/emp-modal/emp-modal.comp.ts
+++ WebClient/src/app/components/employee/emp-modal/emp-modal.comp.ts
@@ -18,26 +18,27 @@ export class EmployeeModalComponent implements OnInit {
   photoFilePath: string;
   fileToUpload: File;
   formData: FormData;
+  buttonClicked: boolean;
 
-  constructor(private service: SharedService) {
-    this.fileToUpload = null;
-    this.formData = new FormData();
-  }
+  constructor(private service: SharedService) {}
 
   ngOnInit(): void {
-    this.loadDepartmentList()
     this.employeeId = this.emp.EmployeeId;
     this.employeeName = this.emp.EmployeeName;
     this.department = this.emp.Department;
     this.dateOfJoining = this.emp.DateOfJoining;
     this.photoFileName = this.emp.PhotoFileName;
     this.photoFilePath = this.service.PhotoUrl + this.photoFileName;
+    this.fileToUpload = null;
+    this.formData = new FormData();
+    this.buttonClicked = false;
+    this.loadDepartmentList();
   }
 
   loadDepartmentList(): void {
-    this.service.getAllDepartmentNamesFromDB().subscribe((response: string[]) => {
-      this.departmentList = response;
-    });
+    this.service.getAllDepartmentNamesFromDB().subscribe((response: string[]) =>
+      this.departmentList = response
+    );
   }
 
   private getEmployee(): IEmployee {
@@ -51,15 +52,15 @@ export class EmployeeModalComponent implements OnInit {
   }
 
   addEmployee(): void {
-    this.service.addEmployeeToDB(this.getEmployee()).subscribe((response: string) => {
-      alert(response);
-    });
+    this.service.addEmployeeToDB(this.getEmployee()).subscribe((response: string) =>
+      alert(response)
+    );
   }
 
   updateEmployee(): void {
-    this.service.updateEmployeeToDB(this.getEmployee()).subscribe(() => {
-      console.warn(this.photoFileName, this.photoFilePath);
-    });
+    this.service.updateEmployeeToDB(this.getEmployee()).subscribe(() =>
+      console.warn(this.photoFilePath)
+    );
   }
 
   onFileSelected(event: any): void {
@@ -73,27 +74,32 @@ export class EmployeeModalComponent implements OnInit {
     console.log(this.photoFileName, this.photoFilePath, this.fileToUpload);
   }
 
-  uploadPhoto(): void {
-    this.service.uploadPhotoToStorage(this.formData).subscribe((response: string) => {
-      try {
+  uploadEmployeeData(): void {
+    this.service.uploadPhotoToStorage(this.formData).subscribe(
+      (response: string) => {
         this.photoFileName = response;
         this.photoFilePath = this.service.PhotoUrl + this.photoFileName;
-        console.warn('Photo upload')
-      } catch (e) {
-        e.console.error('Photo not upload')
-      }
-    });
+        console.warn(response)
+      },
+      (error: string) => console.error(error)
+    );
   }
 
-  updatePhoto(employeeId: number): void {
-    this.service.updatePhotoToStorage(employeeId, this.formData).subscribe((response: string) => {
-      try {
-        this.photoFileName = response;
-        this.photoFilePath = this.service.PhotoUrl + this.photoFileName;
-        console.warn('Photo update')
-      } catch (e) {
-        e.console.error('Photo not update')
-      }
-    });
+  updateEmployeeData(employeeId: number): void {
+    this.service.updatePhotoToStorage(employeeId, this.formData).subscribe(
+      (response: string) => {
+        if (response == 'anonymous.png') {
+          this.photoFileName = this.emp.PhotoFileName;
+          this.photoFilePath = this.service.PhotoUrl + this.photoFileName;
+          this.updateEmployee();
+        } else {
+          this.photoFileName = response;
+          this.photoFilePath = this.service.PhotoUrl + this.photoFileName;
+          this.updateEmployee()
+        }
+        console.warn(response)
+      },
+      (error: string) => console.error(error)
+    );
   }
 }
