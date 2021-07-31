9d9ccae0f17bd4411dc3d6cba2e411a1fec0875f
i-moonlight <infinite.moonlight0506@gmail.com>
2021-07-31 17:48:23 +0200

    test: test return department list from database in shared service

--- WebClient/src/app/services/shared/shared.service.spec.ts
+++ WebClient/src/app/services/shared/shared.service.spec.ts
@@ -1,19 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { SharedService } from './shared.service';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { IDepartment } from '../../components/department/dep.comp';
 
 describe('SharedService', () => {
   let service: SharedService;
+  let formData: FormData;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, HttpClientTestingModule]
     });
     service = TestBed.inject(SharedService);
+    formData = new FormData();
   });
 
   it('should create shared service', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return department list from database', inject([SharedService, HttpTestingController],
+    (service: SharedService, backend: HttpTestingController) => {
+      const mockDepartmentList: IDepartment[] = [];
+      service.getDepartmentListFromDB().subscribe((response: IDepartment[]) =>
+        expect(response).toEqual(mockDepartmentList));
+      backend.expectOne({
+        method: 'GET',
+        url: 'http://localhost:5000/api/department'
+      })
+        .flush(mockDepartmentList);
+    })
+  );
 });
