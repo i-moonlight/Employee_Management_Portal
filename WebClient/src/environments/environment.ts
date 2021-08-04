31073d81cc49141e0bbc04f69e8581f079112c74
i-moonlight <infinite.moonlight0506@gmail.com>
2021-08-04 19:47:20 +0200

    feat(client): authentication service

--- WebClient/src/environments/environment.ts
+++ WebClient/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  authApi: 'http://localhost:4021/',
+  tokenWhitelistedDomains: 'http://localhost:5000'
 };
 
 /*
