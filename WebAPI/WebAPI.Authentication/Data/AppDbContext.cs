d08d9d9f2c52223b3eeb1de745fc94a4969648a2
i-moonlight <infinite.moonlight0506@gmail.com>
2021-08-06 20:38:06 +0200

    refactor: authentication project

---
+++ WebAPI/WebAPI.Authentication/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Authentication.Data.Entities;
+
+namespace WebAPI.Authentication.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {}
+    }
+}
---
+++ WebAPI/WebAPI.Authentication/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebAPI.Authentication.Data;
+using WebAPI.Authentication.Data.Entities;
+
+namespace WebAPI.Authentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(opts =>
+                opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+            
+            services.AddIdentity<AppUser,IdentityRole>(opts => {})
+                .AddEntityFrameworkStores<AppDbContext>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
