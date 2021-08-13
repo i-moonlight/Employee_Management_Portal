bda00521af08b6a1c5b831e77290fe5e6a88a954
i-moonlight <infinite.moonlight0506@gmail.com>
2021-08-13 20:05:18 +0200

    test: test display add employee button in employee list component

---
+++ WebClient/e2e/src/employee/emp-list/emp-list.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { browser, logging } from 'protractor';
+import { EmployeeListPage } from './emp-list.po';
+
+describe('EmployeeListPage ', () => {
+  let page: EmployeeListPage;
+
+  beforeEach(() =>
+    page = new EmployeeListPage());
+
+  it('should display add employee button on page', async () => {
+    await page.navigateTo();
+    expect(await page.getAddEmpButtonDisplayed()).toBeTruthy('Add button is display');
+  });
+
+  afterEach(async () => {
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining(
+      {level: logging.Level.SEVERE} as logging.Entry))
+  });
+});
+
---
+++ WebClient/e2e/src/employee/emp-list/emp-list.po.ts
@@ -0,0 +1,16 @@
+import { browser, by, element, ElementFinder } from 'protractor';
+
+export class EmployeeListPage {
+  async navigateTo(): Promise<unknown> {
+    return browser.get('http://localhost:4200/employee');
+  }
+
+  // Add employee button as trigger modal.
+  getAddEmployeeButton(): ElementFinder {
+    return element(by.css('.btn-float'));
+  }
+
+  async getAddEmpButtonDisplayed(): Promise<boolean> {
+    return (await this.getAddEmployeeButton()).isDisplayed();
+  }
+}
