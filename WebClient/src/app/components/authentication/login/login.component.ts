225b6b0c5fd9b12033358c1c58749fe5e80708c3
i-moonlight <infinite.moonlight0506@gmail.com>
2021-08-17 19:24:04 +0200

    refactor(client): login component

--- WebClient/src/app/components/authentication/login/login.component.ts
+++ WebClient/src/app/components/authentication/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/authentication/auth.service';
 import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Constants } from '../../../common/constants';
+import { ToastrService } from 'ngx-toastr';
+import { Login } from '../../../models/login.model';
+import { Response } from '../../../models/response.model';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +12,23 @@ import { Constants } from '../../../common/constants';
 })
 export class LoginComponent implements OnInit {
 
-  isLoginError: boolean = false;
-
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private toastr: ToastrService, private router: Router) {}
 
   ngOnInit() {}
 
   onSubmit(userName, password) {
-    this.authService.toUserAuthentication(userName, password).subscribe((data: any) => {
-        localStorage.setItem('userToken', data.access_token);
-        this.router.navigate([Constants.BASE_URL + 'employee']);
-      },
-      (err: HttpErrorResponse) => {
-        this.isLoginError = true;
-      });
+    const login: Login = {
+      UserName: userName,
+      Password: password,
+    }
+    this.authService.getLoginToken(login).subscribe((res: Response) => {
+      if (res.DateSet != null) {
+        this.toastr.success('Login Successful', null, {timeOut: 50000});
+        localStorage.setItem('token', res.DateSet.Token);
+        this.router.navigate(['/employee']).then(() => window.location.reload());
+      } else {
+        this.toastr.error('Login Failed');
+      }
+    });
   }
 }
--- WebClient/src/app/services/shared/shared.service.ts
+++ WebClient/src/app/services/shared/shared.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../../components/employee/employee.component';
 import { Department } from '../../components/department/department.component';
@@ -7,7 +7,6 @@ import { Department } from '../../components/department/department.component';
 @Injectable({
   providedIn: 'root'
 })
-
 export class SharedService {
   readonly APIUrl = 'http://localhost:8080';
   readonly PhotoUrl = 'http://localhost:8080/Photos/';
@@ -32,6 +31,13 @@ export class SharedService {
 
   getEmployeeListFromDB(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.APIUrl + '/employee');
+
+  // Retrieves a list of employees from the resource server database using an access token.
+  public getEmployeeListFromDB(): Observable<Employee[]> {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer' + localStorage.getItem('token')
+    });
+    return this.http.get<Employee[]>(this.APIUrl + '/employee', { headers: headers });
   }
 
   getAllDepartmentNamesFromDB(): Observable<string[]> {
