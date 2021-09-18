df03108dbbf895c51a7af7af2ce93842dc9a4467
i-moonlight <infinite.moonlight0506@gmail.com>
2021-09-18 19:16:27 +0200

    refactor: authentication component

--- WebClient/src/app/components/authentication/auth.component.ts
+++ WebClient/src/app/components/authentication/auth.component.ts
@@ -5,7 +5,7 @@ import { SharedService } from '../../services/shared/shared.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
 
