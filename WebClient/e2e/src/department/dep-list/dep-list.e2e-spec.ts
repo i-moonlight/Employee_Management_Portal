e2b65f20ad4c1a7162e215581f42a8fd3986e508
i-moonlight <infinite.moonlight0506@gmail.com>
2021-10-21 19:21:32 +0200

    test(client): department list tests

---
+++ WebClient/e2e/src/department/dep-list/dep-list.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { DepartmentListPage } from './dep-list.po';
+import { browser, logging } from 'protractor';
+
+describe('DepartmentListPage', () => {
+  let page: DepartmentListPage;
+
+  beforeEach(() =>
+    page = new DepartmentListPage());
+
+  it('should display add department button on page', async () => {
+    await page.navigateTo();
+    expect(await page.isAddDepButtonDisplayed()).toBeTruthy('Add button is display');
+  });
+
+  it('should display add department button name as `Add Department` on page', async () => {
+    await page.navigateTo();
+    expect(await page.getAddDepartmentButtonName()).toEqual('Add Department');
+  });
+
+  it('should display modal window on page when click on add department button', async () => {
+    await page.navigateTo();
+    await page.getAddDepButton().click();
+    await browser.sleep(1000);
+    expect(await page.isModalDisplayed()).toBeTruthy('Modal window is open');
+  });
+
+  it('should display modal title as `Add Department` on page when click on add department button', async () => {
+    await page.navigateTo();
+    await page.getAddDepButton().click();
+    await browser.sleep(1000);
+    expect(await page.getModalTitle()).toEqual('Add Department');
+  });
+
+  it('should display close modal button when click on add department button', async () => {
+    await page.navigateTo();
+    await page.getAddDepButton().click();
+    await browser.sleep(1000);
+    expect(await page.isCloseModalButtonDisplayed()).toBeTruthy('Close button is display');
+  });
+
+  it('should close modal window when click on close button', async () => {
+    await page.navigateTo();
+    await page.getAddDepButton().click();
+    await browser.sleep(1000);
+    await page.getCloseModalButton().click();
+    await browser.sleep(1000);
+    expect(await page.isModalDisplayed()).toBeFalsy('Modal window is close');
+  });
+
+  it('should display modal component when click on add department button', async () => {
+    await page.navigateTo();
+    await page.getAddDepButton().click();
+    await browser.sleep(1000);
+    expect(await page.isModalComponentDisplayed()).toBeTruthy('Modal component is display');
+  });
+
+  afterEach(async () => {
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining(
+      {level: logging.Level.SEVERE} as logging.Entry));
+  });
+})
---
+++ WebClient/e2e/src/department/dep-list/dep-list.po.ts
@@ -0,0 +1,51 @@
+import { browser, by, element, ElementFinder } from 'protractor';
+
+export class DepartmentListPage {
+  async navigateTo(): Promise<unknown> {
+    return browser.get('http://localhost:4200/department');
+  }
+
+  // Add department button as trigger modal
+  getAddDepButton(): ElementFinder {
+    return element(by.css('.btn-float'));
+  }
+
+  async isAddDepButtonDisplayed(): Promise<boolean> {
+    return (await this.getAddDepButton()).isDisplayed();
+  }
+
+  async getAddDepartmentButtonName(): Promise<string> {
+    return (await this.getAddDepButton()).getText();
+  }
+
+  // Modal window
+  getModal(): ElementFinder {
+    return element(by.id('exampleModal'));
+  }
+
+  async isModalDisplayed(): Promise<boolean> {
+    return (await this.getModal()).isDisplayed();
+  }
+
+  async getModalTitle(): Promise<string> {
+    return element(by.className('modal-title')).getText();
+  }
+
+  // Close modal button
+  getCloseModalButton(): ElementFinder {
+    return element(by.className('btn-close'));
+  }
+
+  async isCloseModalButtonDisplayed(): Promise<boolean> {
+    return (await this.getCloseModalButton()).isDisplayed();
+  }
+
+  // Modal component
+  getModalComponent(): ElementFinder {
+    return element(by.tagName('app-dep-modal'));
+  }
+
+  async isModalComponentDisplayed(): Promise<boolean> {
+    return (await this.getModalComponent()).isDisplayed();
+  }
+}
