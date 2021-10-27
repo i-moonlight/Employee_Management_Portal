177d0274589c557d4128162f9608417ecd5210ef
i-moonlight <infinite.moonlight0506@gmail.com>
2021-10-27 19:20:01 +0200

    test(client): department modal tests

---
+++ WebClient/e2e/src/department/dep-modal/dep-modal.e2e-spec.ts
@@ -0,0 +1,85 @@
+import { browser, logging } from 'protractor';
+import { DepartmentModalPage } from './dep-modal.po';
+
+describe('DepartmentModalPage', () => {
+  let page: DepartmentModalPage;
+
+  beforeEach(() =>
+    page = new DepartmentModalPage());
+
+  it('should display department name label as `Department Name` on page when click on add button', async () => {
+    await page.navigateTo();
+    await page.getAddDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getAddDepartmentLabel()).toEqual('Department Name');
+  });
+
+  it('should display department name input on page when click on add button', async () => {
+    await page.navigateTo();
+    await page.getAddDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.isDepartmentNameInputDisplayed()).toBeTruthy('Department name input is display');
+  });
+
+  it('should display department name placeholder as `Enter Department Name` on page when click on add button', async () => {
+    await page.navigateTo();
+    await page.getAddDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getDepartmentNameInputPlaceholder()).toEqual('Enter Department Name');
+  });
+
+  it('should display add department modal button on page when click on add button', async () => {
+    await page.navigateTo();
+    await page.getAddDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.isAddModalButtonDisplayed()).toBeTruthy('Add department modal button is display');
+  });
+
+  it('should display add department modal button label as `ADD` on page when click on button', async () => {
+    await page.navigateTo();
+    await page.getAddDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getAddDepModalLabel()).toEqual('ADD');
+  });
+
+  it('should display department name label as `Department Name` on page when click on edit button', async () => {
+    await page.navigateTo();
+    await page.getEditDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getAddDepartmentLabel()).toEqual('Department Name');
+  });
+
+  it('should display department name input on page when click on edit button', async () => {
+    await page.navigateTo();
+    await page.getEditDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.isDepartmentNameInputDisplayed()).toBeTruthy('Department name input is display');
+  });
+
+  it('should display department name placeholder as `Enter Department Name` on page when click on edit button', async () => {
+    await page.navigateTo();
+    await page.getEditDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getDepartmentNameInputPlaceholder()).toEqual('Enter Department Name');
+  });
+
+  it('should display update department modal button on page when click on edit button', async () => {
+    await page.navigateTo();
+    await page.getEditDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.isUpdateDepartmentButtonDisplayed()).toBeTruthy('Update department modal button is display');
+  });
+
+  it('should display update department modal button name as `UPDATE` on page when click on edit button', async () => {
+    await page.navigateTo();
+    await page.getEditDepartmentButton().click();
+    await browser.sleep(1000);
+    expect(await page.getUpdateDepartmentButtonName()).toEqual('UPDATE');
+  });
+
+  afterEach(async () => {
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining(
+      {level: logging.Level.SEVERE} as logging.Entry))
+  });
+});
