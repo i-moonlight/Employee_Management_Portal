ac539c0ac82fcbba946bf2bb0eac5e14b611b003
i-moonlight <infinite.moonlight0506@gmail.com>
2021-10-27 19:22:14 +0200

    refactor: authentication project

---
+++ WebAPI/WebAPI.Authentication/Data/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Authentication.Data.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI.Authentication/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAPI.Authentication
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
