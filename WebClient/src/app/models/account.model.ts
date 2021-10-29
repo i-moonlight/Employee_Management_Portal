1da6256c7eb9057a8ec6f0ddba1f9b29cd6fa6df
i-moonlight <infinite.moonlight0506@gmail.com>
2021-10-29 19:08:21 +0200

    refactor(client): app web client

--- WebClient/src/app/models/account.model.ts
+++
@@ -1,7 +0,0 @@
-export class Account {
-  public UserId?: string;
-  public UserName?: string;
-  public Email?: string;
-  public Password?: string;
-  public ResetPasswordUrl?: string;
-}
--- WebClient/src/app/models/login.model.ts
+++
@@ -1,4 +0,0 @@
-export class Login {
-  public UserName: string;
-  public Password: string;
-}
