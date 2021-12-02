2bc6d277c3c3ea375ee369c44649b5ad3cbbd56d
i-moonlight <infinite.moonlight0506@gmail.com>
2021-12-02 20:11:17 +0200

    feat(client): product rating

--- client/web/README.md
+++ client/web/README.md
@@ -6,9 +6,9 @@ This project was bootstrapped with [Create App](http://vanilla-js.com/).
 
 In the project directory, you can run:
 
-### `npm init`
+#### `npm init`
 
-### `npm install -d live-server`
+#### `npm install -d live-server`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
@@ -16,7 +16,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm install -D webpack webpack-cli webpack-dev-server`
+#### `npm install -D webpack webpack-cli webpack-dev-server`
 Install webpack
 
 ```json
@@ -27,4 +27,23 @@ package.json
 "start": "webpack-dev-server --port=3000 --open",
 "test": "echo \"Error: no test specified\" && exit 1"
 }
-``
+```
+
+### install jslint by iDEA plugin
+Установка линтера через плагин другие установки и настройки избыточны.
+
+Очистка кэша
+#### `npm cache clean --force`
+#### `npm cache verify`
+
+Удаление неиспользуемых модулей в папке node_modules
+#### `npm install rimraf -g`
+#### `npx rimraf node_modules`
+#### `npm ci`
+
+
+Очистка неиспользуемых зависимостей
+#### `npm prune`
+
+Вывод список используемых зависимостей
+#### `npm ls`
--- client/web/src/pages/HomePage.js
+++ client/web/src/pages/HomePage.js
@@ -3,36 +3,51 @@ import { APP_BASE_URL } from '../api/App.constants';
 
 const HomePage = {
     render: async () => {
-        const response = await axios({
-            url: APP_BASE_URL + 'product',
+        const [response] = await Promise.all([axios({
+            url: APP_BASE_URL + `product`,
             headers: {
                 'Content-Type': 'application/json',
             },
-        });
+        })]);
         if (!response || response.statusText !== 'OK') {
-            return `<div>Error in getting data</div>`;
+            return '<div>Error in getting data</div>';
         }
         const products = response.data;
 
-        return
-        ` <ul class="products">
-           ${products.map((product) => `
-         <li>
-           <div class="product">
-             <a href="/#/product/${product._id}">
-               <img src="${product.image}" alt="${product.name}" />
-             </a>
-             <div class="product-name">
-               <a href="/#/product/1">${product.name}</a>
-             </div>
-             <div class="product-brand">${product.brand}</div>
-             <div class="product-price">$${product.price}</div>
-           </div>
-         </li>
-      `
-        ).join('\n')}
-      `;
-
-    },
+        return `
+        <ul class="products">
+          ${products.map((product) => `
+          <li>
+            <div class="product">
+              <a href="/#/product/${product._id}">
+                <img src="${product.image}" alt="${product.name}" />
+              </a>
+              
+                <div class="product-name">
+                  <a href="/#/product/1">
+                    ${product.name}
+                  </a>
+                </div>
+            
+                <div class="product-rating">
+                  ${Rating.render({
+                            value: product.rating,
+                            text: `${product.numReviews} reviews`,
+                        })}
+                </div>
+                
+                <div class="product-brand">
+                  ${product.brand}
+                </div>
+                
+                <div class="product-price">
+                  $${product.price}
+                </div>
+            </div>
+          </li>
+          `
+          ).join('\n')}
+        `;
+        },
 };
 export default HomePage;
