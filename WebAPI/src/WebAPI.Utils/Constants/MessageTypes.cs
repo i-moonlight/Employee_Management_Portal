9c64f91707f008f3c28fec652f0856eb5d7e8b92
i-moonlight <infinite.moonlight0506@gmail.com>
2021-12-08 19:10:42 +0200

    feat: web application testing factory
     - Web application factory for controller api integration testing

---
+++ WebAPI/src/WebAPI.Utils/Constants/MessageTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAPI.Utils.Constants
+{
+    public static class MessageTypes
+    {
+        public const string ErrorMessage = @"An error occured seeding the database with test messages, Error:";
+    }
+}
