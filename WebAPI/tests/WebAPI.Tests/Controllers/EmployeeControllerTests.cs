d6eec8b6a9396f93bbc49390d9a6a167cb75f618
i-moonlight <infinite.moonlight0506@gmail.com>
2021-12-19 19:48:34 +0200

    test: integration test delete employee method status code

--- WebAPI/tests/WebAPI.Tests/Controllers/EmployeeControllerTests.cs
+++ WebAPI/tests/WebAPI.Tests/Controllers/EmployeeControllerTests.cs
@@ -228,5 +228,24 @@ namespace WebAPI.Tests.Controllers
             // Assert.
             Assert.AreEqual(typeof(Task<ActionResult<string>>), result.GetType());
         }
+
+        [Test]
+        public async Task DeleteEmployee_Method_Should_Returns_Success_Http_Status_Code()
+        {
+            // Arrange.
+            var client = _factory.CreateClient();
+            var dbContext = _factory.Services.CreateScope().ServiceProvider.GetService<AppDbContext>();
+            await dbContext.Employees.AddRangeAsync(new Employee());
+            await dbContext.SaveChangesAsync();
+            var employeeId = dbContext.Employees?.FirstOrDefault()?.Id;
+
+            // Act.
+            var response = await client.DeleteAsync($"api/employee/{employeeId}");
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            // Assert.
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("Deleted successfully", stringResponse);
+        }
     }
 }
