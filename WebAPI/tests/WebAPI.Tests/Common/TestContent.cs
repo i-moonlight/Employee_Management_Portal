adb5f65bb14f0ececc0ec63c467ee0dbe19cadd6
i-moonlight <infinite.moonlight0506@gmail.com>
2021-12-20 17:14:05 +0200

    test: test get employee list query handler

--- WebAPI/tests/WebAPI.Tests/Common/TestContent.cs
+++ WebAPI/tests/WebAPI.Tests/Common/TestContent.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using WebAPI.Entities.Models;
 using WebAPI.UserCases.Common.Dto;
 
 namespace WebAPI.Tests.Common
 {
-    public class TestContent
+    public static class TestContent
     {
         public static EmployeeDto GetTestEmployeeDto() => new EmployeeDto()
         {
@@ -17,16 +20,22 @@ namespace WebAPI.Tests.Common
             PhotoFileName = "PhotoFileName"
         };
 
+        public static IEnumerable GetTestEmployeeList() => new List<Employee>
+            {
+                new Employee
+                {
+                    Id = new Guid("9EAA21E2-3DF3-4C32-7EE5-08DA1DB6A7E3"),
+                    Name = "Name",
+                    Department = "Department",
+                    DateOfJoining = DateTime.UtcNow,
+                    PhotoFileName = "PhotoFileName"
+                }
+            }
+            .AsEnumerable();
+
         public static StringContent GetRequestContent(object obj)
         {
             return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
-        }
-
-        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
-        {
-            var stringResponse = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<T>(stringResponse);
-            return result;
         }
     }
 }
