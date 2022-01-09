4063a84b3953b30ea626232783b297d731e1ae73
i-moonlight <infinite.moonlight0506@gmail.com>
2022-01-09 19:13:16 +0200

    test: refactor test update department method in department controller

--- WebAPI/tests/WebAPI.Tests/Controllers/DepartmentControllerTests.cs
+++ WebAPI/tests/WebAPI.Tests/Controllers/DepartmentControllerTests.cs
@@ -127,20 +127,19 @@ namespace WebAPI.Tests.Controllers
             Assert.AreEqual("Name must be filled", stringResponse);
         }
 
-        // [Test]
-        // public void Put_Should_Returns_JsonResult_String_Value()
-        // {
-        //     // Arrange. 
-        //     _mockDepRepo.Setup(x => x.Update(_model)).Returns(_fakeCategories.First());
-        //
-        //     // Act.
-        //     var result = _controller.Put(_fakeCategories.First());
-        //
-        //     // Assert.
-        //     Assert.NotNull(result, "Result is null");
-        //     Assert.AreEqual(new JsonResult(_model).GetType(), result.GetType(), "Return type mismatch");
-        //     Assert.AreEqual(typeof(string), result.Value.GetType(), "Return value type mismatch");
-        // }
+        [Test]
+        public void UpdateDepartment_Method_Should_Returns_ActionResult_String_Type()
+        {
+            // Arrange.
+            var departmentDto = TestContent.GetTestDepartmentDto();
+
+            // Act.
+            var result = _controller.UpdateDepartment(departmentDto);
+
+            // Assert.
+            Assert.AreEqual(typeof(Task<ActionResult<string>>), result.GetType());
+        }
+
         //
         // [Test]
         // public void Delete_Should_Returns_JsonResult_String_Value()
