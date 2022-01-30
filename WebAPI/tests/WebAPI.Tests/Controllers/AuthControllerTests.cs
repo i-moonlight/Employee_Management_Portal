39d65d2f41ce91a839bb4c35e0d65e81b4162b2a
i-moonlight <infinite.moonlight0506@gmail.com>
2022-01-30 18:06:31 +0200

    test: test get user list method in authentication controller

---
+++ WebAPI/tests/WebAPI.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests.Controllers
+{
+    [TestFixture]
+    public class AuthControllerTests
+    {
+        private AuthController _authController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _authController = new AuthController();
+        }
+
+        [Test]
+        public void GetUserList_Method_Should_Returns_ActionResult_IEnumerable_Type()
+        {
+            // Act.
+            var result = _authController.GetUserList();
+
+            // Assert.
+            Assert.AreEqual(typeof(Task<ActionResult<IEnumerable>>), result.GetType());
+        }
+    }
+}
