2bef33f215415601fc40cf11fd7ef489fd53e9ef
i-moonlight <infinite.moonlight0506@gmail.com>
2022-02-03 20:26:34 +0200

    test: test get user list query handler method

---
+++ WebAPI/tests/WebAPI.Tests/Common/TestManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace WebAPI.Tests.Common
+{
+    /// <summary>
+    /// Sets access to mock objects for testing.
+    /// </summary>
+    public static class TestManager
+    {
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>(IUserStore<TUser> store = null)
+            where TUser : class
+        {
+            var mockStore = new Mock<IUserPasswordStore<TUser>>();
+
+            var mockUserManager = new Mock<UserManager<TUser>>(
+                mockStore.Object, null, null, null, null, null, null, null, null);
+
+            mockUserManager
+                .Setup(um => um.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            return mockUserManager;
+        }
+    }
+}
---
+++ WebAPI/tests/WebAPI.Tests/Requests/Queries/AuthQueryHandlersTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebAPI.Entities.Models;
+using WebAPI.Tests.Common;
+using WebAPI.UserCases.Requests.Authentication.Queries;
+
+namespace WebAPI.Tests.Requests.Queries
+{
+    [TestFixture]
+    public class AuthQueryHandlersTests : RequestTestSetup
+    {
+        private GetUserListQuery _request;
+
+        [SetUp]
+        public new void Setup()
+        {
+            _request = new GetUserListQuery();
+        }
+
+        [Test]
+        public async Task GetUserListQueryHandler_Handler_Method_Should_Returns_User_List()
+        {
+            // Arrange
+            var manager = TestManager.MockUserManager<User>().Object;
+            var handler = new GetUserListQueryHandler(manager);
+
+            // Act.
+            var result = await handler.Handle(_request, CancellationToken.None);
+
+            // Assert.
+            Assert.IsEmpty(result);
+        }
+    }
+}
