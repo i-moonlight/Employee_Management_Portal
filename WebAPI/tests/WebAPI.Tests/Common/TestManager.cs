f41c9a7feca15465d3378b1c248ae9bc48094752
i-moonlight <infinite.moonlight0506@gmail.com>
2022-02-04 18:26:39 +0200

    test: test the user registration command handler method on success

--- WebAPI/tests/WebAPI.Tests/Common/TestManager.cs
+++ WebAPI/tests/WebAPI.Tests/Common/TestManager.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 
 namespace WebAPI.Tests.Common
---
+++ WebAPI/tests/WebAPI.Tests/Requests/Commands/AuthCommandHandlersTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebAPI.Entities.Models;
+using WebAPI.Tests.Common;
+using WebAPI.UserCases.Requests.Authentication.Commands;
+
+namespace WebAPI.Tests.Requests.Commands
+{
+    [TestFixture]
+    public class AuthCommandHandlersTests : RequestTestSetup
+    {
+        [Test]
+        public async Task RegisterUserCommandHandler_Handle_Method_Should_Returns_Success_String()
+        {
+            // Arrange.
+            var testRegisterUserDto = TestContent.GetTestRegisterUserDto();
+            var request = new RegisterUserCommand() {RegisterUserDto = testRegisterUserDto};
+            var manager = TestManager.MockUserManager<User>();
+            var handler = new RegisterUserCommandHandler(manager.Object, Mapper);
+
+            // Act.
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert.
+            Assert.AreEqual("User has been registered", result.ResponseMessage);
+        }
+    }
+}
