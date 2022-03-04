84372e3dc1c5cf061b418e14e19b53ba994ed56b
i-moonlight <infinite.moonlight0506@gmail.com>
2022-03-04 19:34:06 +0200

    feat(client): api interceptor

---
+++ client/src/api/api.interceptor.ts
@@ -0,0 +1,36 @@
+import axios, { CreateAxiosDefaults } from 'axios';
+import { getAccessToken, removeFromStorage } from '@/services/auth/auth.helper';
+import { AuthService } from '@/services/auth/auth.service';
+import { errorCatch, getContentType } from './api.helper';
+
+const axiosOptions: CreateAxiosDefaults = {
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+	headers: getContentType(),
+}
+
+export const instance = axios.create(axiosOptions);
+
+instance.interceptors.request.use(async config => {
+	const accessToken = getAccessToken();
+	if (config.headers && accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+	return config;
+})
+
+instance.interceptors.response.use(
+	config => config,
+	async error => {
+		const originalRequest = error.config;
+		if (error?.response?.status === 401 || errorCatch(error) === 'jwt expired' ||
+			(errorCatch(error) === 'jwt must be provided' && error.config && !error.config._isRetry))
+		{
+			originalRequest._isRetry = true;
+			try {
+				await AuthService.getNewTokens();
+				return instance.request(originalRequest);
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage();
+			}
+		}
+		throw error;
+	}
+);
---
+++ client/src/store/user/user.interface.ts
@@ -0,0 +1,23 @@
+export interface UserState {
+	email: string;
+	isAdmin: boolean;
+}
+
+export interface Tokens {
+	accessToken: string;
+	refreshToken: string;
+}
+
+export interface InitialState {
+	user: UserState | null;
+	isLoading: boolean;
+}
+
+export interface EmailPassword {
+	email: string;
+	password: string;
+}
+
+export interface AuthResponse extends Tokens {
+	user: User;
+}
