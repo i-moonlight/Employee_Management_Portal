b8e2a081fe3ac914542363e5cb012407966047ee
i-moonlight <infinite.moonlight0506@gmail.com>
2022-03-16 20:06:14 +0200

    feat(client): auth service

--- client/src/api/api.interceptor.ts
+++ client/src/api/api.interceptor.ts
@@ -1,11 +1,12 @@
 import axios, { CreateAxiosDefaults } from 'axios';
-import { getAccessToken, removeFromStorage } from '@/services/auth/auth.helper';
 import { AuthService } from '@/services/auth/auth.service';
+import { getAccessToken, removeFromStorage } from '@/services/auth/token.service';
+
 import { errorCatch, getContentType } from './api.helper';
 
 const axiosOptions: CreateAxiosDefaults = {
 	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	headers: getContentType(),
+	headers: getContentType()
 }
 
 export const instance = axios.create(axiosOptions);
@@ -14,12 +15,14 @@ instance.interceptors.request.use(async config => {
 	const accessToken = getAccessToken();
 	if (config.headers && accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
 	return config;
-})
+});
+
 
 instance.interceptors.response.use(
 	config => config,
 	async error => {
 		const originalRequest = error.config;
+
 		if (error?.response?.status === 401 || errorCatch(error) === 'jwt expired' ||
 			(errorCatch(error) === 'jwt must be provided' && error.config && !error.config._isRetry))
 		{
