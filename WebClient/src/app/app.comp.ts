b8877e2826fa96d79a986850ac215abd66677f08
i-moonlight <infinite.moonlight0506@gmail.com>
2022-03-19 20:41:04 +0200

    feat(client): progress bar in header

--- WebClient/src/app/app.comp.ts
+++ WebClient/src/app/app.comp.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AuthService} from "./services/authentication/auth.service";
+import { AuthService } from "@services/authentication/auth.service";
 
 @Component({
   selector: 'app-root',
--- WebClient/src/app/modules/app.module.ts
+++ WebClient/src/app/modules/app.module.ts
@@ -4,24 +4,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { NgProgressModule } from '@ngx-progressbar/core';
 import { ToastrModule } from 'ngx-toastr';
-
 import { AppComponent } from '../app.comp';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from '@auth/auth.component';
 import { AuthGuard } from '@guards/auth.guard';
 import { AuthInterceptor } from '@guards/auth.interceptor';
 import { AuthService } from '@services/authentication/auth.service';
-import { DepartmentComponent } from '../components/department/dep.comp';
-import { DepartmentListComponent } from '../components/department/dep-list/dep-list.comp';
-import { DepartmentModalComponent} from '../components/department/dep-modal/dep-modal.comp';
-import { EmployeeComponent } from '../components/employee/emp.comp';
-import { EmployeeListComponent } from '../components/employee/emp-list/emp-list.comp';
-import { EmployeeModalComponent } from '../components/employee/emp-modal/emp-modal.comp';
+import { DepartmentComponent } from '@components/department/dep.comp';
+import { DepartmentListComponent } from '@components/department/dep-list/dep-list.comp';
+import { DepartmentModalComponent} from '@components/department/dep-modal/dep-modal.comp';
+import { EmployeeComponent } from '@components/employee/emp.comp';
+import { EmployeeListComponent } from '@components/employee/emp-list/emp-list.comp';
+import { EmployeeModalComponent } from '@components/employee/emp-modal/emp-modal.comp';
 import { ForgotPasswordComponent } from '@auth/forgot-password/forgot-password.component';
+import { HeaderComponent } from '@components/elements/header/header.component';
 import { LoginComponent } from '@auth/login/login.component';
-import { ManagerComponent } from '../components/manager/manager.component';
+import { ManagerComponent } from '@components/manager/manager.component';
+import { ProgressBarService } from '@services/progress-bar/progress-bar.service';
 import { RegisterComponent } from '@auth/registration/register.component';
 import { SharedService } from '@services/shared/shared.service';
 
@@ -36,11 +37,11 @@ import { SharedService } from '@services/shared/shared.service';
     EmployeeListComponent,
     EmployeeModalComponent,
     ForgotPasswordComponent,
+    HeaderComponent,
     LoginComponent,
     ManagerComponent,
     RegisterComponent,
   ],
-
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -50,15 +51,15 @@ import { SharedService } from '@services/shared/shared.service';
     ReactiveFormsModule,
     RouterModule,
     ToastrModule.forRoot(),
+    NgProgressModule,
   ],
-
   providers: [
     AuthGuard,
     AuthService,
     SharedService,
+    ProgressBarService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
   ],
-
   bootstrap: [AppComponent]
 })
 export class AppModule {}
